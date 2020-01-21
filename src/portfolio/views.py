571034e43b561f6b51efa5e78c52e564c077dadc
Pierre BONNAMY <p.bonnamy@intech-sud.fr>
2020-01-22 00:40:10 +0100

    Add views functions

--- src/portfolio/views.py
+++ src/portfolio/views.py
@@ -3,4 +3,20 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello world")
+    return render(request, 'index.html')
+
+
+def all_skills(request):
+    return HttpResponse("All skills")
+
+
+def skill(request, id):
+    return HttpResponse("Skill N°: {}".format(id))
+
+
+def all_projects(request):
+    return HttpResponse("All projects")
+
+
+def project(request, id):
+    return HttpResponse("Project N°: {}".format(id))
