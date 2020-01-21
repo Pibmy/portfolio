7580ec81d04d03b7bce6189fe12c7e6e29f72367
Pierre BONNAMY <p.bonnamy@intech-sud.fr>
2020-01-22 00:44:20 +0100

    Add person model in index view.

--- src/portfolio/views.py
+++ src/portfolio/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Person
+
 
 def index(request):
-    return render(request, 'index.html')
+    user = Person.objects.get(id=1)
+    return render(request, 'index.html', context={'user': user})
 
 
 def all_skills(request):
