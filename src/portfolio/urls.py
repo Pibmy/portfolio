b55dcd8354af1512168b16d940ca5eec54c410f9
Pierre BONNAMY <p.bonnamy@intech-sud.fr>
2020-01-22 00:40:26 +0100

    Add urls endpoints.

--- src/portfolio/urls.py
+++ src/portfolio/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('skills/', views.all_skills),
+    path('skills/<int:id>/', views.skill),
+    path('projects/', views.all_projects),
+    path('projects/<int:id>/', views.project),
 ]
