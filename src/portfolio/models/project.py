6c9f37764fe3f6f8399b6162bd4a4fce622c59db
Pierre BONNAMY <p.bonnamy@intech-sud.fr>
2020-02-16 19:05:48 +0100

    update project model

--- src/portfolio/models/project.py
+++ src/portfolio/models/project.py
@@ -1,12 +1,33 @@
 from django.db import models
 
+from .skill import Skill
+from .person import Person
+from .experience import Formation, Profesion
+
 
 class Project(models.Model):
 
     name = models.CharField(max_length=30)
-
-    date = models.DateField()
-    duration = models.DurationField(blank=True)
-
-    company = models.CharField(max_length=30, blank=True)
     description = models.TextField(blank=True)
+
+    from_date = models.DateField()
+    to_date = models.DateField()
+
+    # relations
+    person = models.ForeignKey(Person, on_delete=models.DO_NOTHING)
+    skills = models.ManyToManyField(Skill, blank=True)
+    pro_context = models.ForeignKey(
+        Profesion,
+        on_delete=models.DO_NOTHING,
+        blank=True,
+        null=True
+    )
+    sch_context = models.ForeignKey(
+        Formation,
+        on_delete=models.DO_NOTHING,
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
