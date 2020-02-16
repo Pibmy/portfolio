f7d03109a9032f23f1bdf316cde2efdddc048ee8
Pierre BONNAMY <p.bonnamy@intech-sud.fr>
2020-02-16 18:59:18 +0100

    register models for admin panel.

--- src/portfolio/admin.py
+++ src/portfolio/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Person, Project, Skill, XP
+from .models import Person, Skill, Formation, Profesion, Project
 
 # Register your models here.
 
@@ -9,16 +9,21 @@ class PersonAdmin(admin.ModelAdmin):
     pass
 
 
-@admin.register(Project)
-class ProjectAdmin(admin.ModelAdmin):
-    pass
-
-
 @admin.register(Skill)
 class SkillAdmin(admin.ModelAdmin):
     pass
 
 
-@admin.register(XP)
-class XPAdmin(admin.ModelAdmin):
+@admin.register(Formation)
+class FormationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Profesion)
+class ProfesionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
     pass
