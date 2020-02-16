29253c0bf4458f69a5b04fa187e073810f8fe1d4
Pierre BONNAMY <p.bonnamy@intech-sud.fr>
2020-02-16 19:05:21 +0100

    Use fr-FR language code.

--- src/app/settings.py
+++ src/app/settings.py
@@ -122,7 +122,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-FR'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
